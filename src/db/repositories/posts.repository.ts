b203cee03f3c42649b2f626ec5b8e894346cc2be
maruhxn_ <77433220+lhk6397@users.noreply.github.com>
2023-08-25 01:58:52 +0900

    feat:Add getAllPosts by sortQuery

--- src/db/repositories/posts.repository.ts
+++ src/db/repositories/posts.repository.ts
@@ -1,54 +1,127 @@
+import CONFIGS from "@/configs/config";
 import { db } from "@/configs/db";
 import {
   CreatePostDto,
   UpdatePostDto,
 } from "@/libs/validators/posts.validator";
-import { eq, like, sql } from "drizzle-orm";
+import { asc, desc, eq, like, sql } from "drizzle-orm";
 import { ulid } from "ulid";
-import { post, vote } from "../schema";
+import { post, reference, user, vote } from "../schema";
+
+type IPOST = {
+  id: string;
+  title: string;
+  content: string;
+  authorId: string;
+  firstChoice: string;
+  secondChoice: string;
+  createdAt: string;
+  updatedAt: string;
+  voteAmt: string;
+  votes: {
+    id: string;
+    isFirstChice: boolean;
+  };
+  reference: {
+    id: string;
+    imagePath: string;
+  };
+  author: {
+    email: string;
+    username: string;
+  };
+};
 
 /** TODO - GET ALL POSTS */
 export const getAllPosts = async (skipPage: number, sortQuery: string) => {
-  // const posts = await db.query.post.findMany({
-  //   extras: {
-  //     voteAmt: sql<number>`count(${vote.id})`.as("vote_amt"),
-  //   },
-  //   with: {
-  //     references: true,
-  //     author: {
-  //       columns: {
-  //         id: true,
-  //         username: true,
-  //       },
-  //     },
-  //     votes: true,
-  //   },
-  //   limit: CONFIGS.POSTS_PAGESIZE,
-  //   offset: skipPage,
-  //   orderBy:
-  //     sortQuery === "popular"
-  //       ? sql`vote_amt DESC`
-  //       : sortQuery === "latest"
-  //       ? desc(post.updatedAt)
-  //       : asc(post.updatedAt),
-  // });
-
-  const posts = await db
+  const results = await db
     .select({
       id: post.id,
       title: post.title,
       content: post.content,
+      authorId: post.authorId,
       firstChoice: post.firstChoice,
       secondChoice: post.secondChoice,
-      authorId: post.authorId,
       createdAt: post.createdAt,
       updatedAt: post.updatedAt,
-      voteAmt: sql<number>`count(${vote.id})`.as("vote_amt"),
+      voteAmt: sql<number>`count(vote.id)`.as("vote_amt"),
+      vote: {
+        id: vote.id,
+        isFirstChice: vote.isFirstChoice,
+      },
+      reference: {
+        id: reference.id,
+        imagePath: reference.imagePath,
+      },
+      author: {
+        email: user.email,
+        username: user.username,
+      },
     })
     .from(post)
+    .innerJoin(user, eq(post.authorId, user.id))
+    .leftJoin(reference, eq(post.id, reference.postId))
     .leftJoin(vote, eq(post.id, vote.postId))
-    .groupBy(post.id, post.title)
-    .orderBy(sql`vote_amt DESC`);
+    .groupBy(post.id, post.title, vote.id, user.id, reference.id)
+    .limit(CONFIGS.POSTS_PAGESIZE)
+    .offset(skipPage)
+    .orderBy(
+      sortQuery === "popular"
+        ? sql`vote_amt DESC`
+        : sortQuery === "latest"
+        ? desc(post.updatedAt)
+        : asc(post.updatedAt)
+    );
+
+  const posts = results.reduce<Record<number, IPOST>>((acc, row) => {
+    const {
+      id,
+      title,
+      content,
+      authorId,
+      firstChoice,
+      secondChoice,
+      createdAt,
+      updatedAt,
+      voteAmt,
+      vote,
+      reference,
+      author,
+    } = row;
+    /** @ts-ignore */
+    let foundObjIndex = acc.findIndex((post) => post.id === id);
+
+    if (foundObjIndex === -1) {
+      /** @ts-ignore */
+      acc.push({
+        id,
+        title,
+        content,
+        authorId,
+        firstChoice,
+        secondChoice,
+        createdAt,
+        updatedAt,
+        voteAmt,
+        votes: [],
+        references: [],
+        author,
+      });
+
+      /** @ts-ignore */
+      foundObjIndex = acc.length - 1;
+      console.log(vote);
+    }
+
+    /** @ts-ignore */
+    if (vote) acc[foundObjIndex].votes.push(vote);
+
+    /** @ts-ignore */
+    if (reference) acc[foundObjIndex].references.push(reference);
+
+    return acc;
+  }, []);
+
   return posts;
 };
 
